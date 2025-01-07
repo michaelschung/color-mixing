d963d3e97ddda38ad281450559ebfa5d41924c37
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-01-06 20:02:58 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+// index.ts
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
